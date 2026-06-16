2060097f92e04981b24f24540b7cff65b8f58cc0
prachi0716 <prachiptl1707@gmail.com>
2026-06-16 08:18:31 -0400

    deleted waste file

--- code front/src/api/aiApi.js
+++
@@ -1,6 +0,0 @@
-import axiosClient from "./axiosClient.js";
-
-export async function sendChatMessage(message) {
-    const response = await axiosClient.post("/ai/chat", { message });
-    return response.data.data;
-}
--- code front/src/api/authApi.js
+++
@@ -1,9 +0,0 @@
-import axiosClient from "./axiosClient.js";
-
-export const registerUser = (userData) => {
-    return axiosClient.post("/auth/register", userData);
-};
-
-export const loginUser = (loginData) => {
-    return axiosClient.post("/auth/login", loginData);
-};
